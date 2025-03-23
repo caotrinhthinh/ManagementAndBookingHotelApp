afd4073d026d250624d098386d4c00763369997b
caotrinhthinh <caotrinhthinh@gmail.com>
2025-03-23 11:08:04 +0700

    Intitial commit

---
+++ src/main/java/com/example/ManagementAndBookingHotelApp/ManagementAndBookingHotelAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ManagementAndBookingHotelApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementAndBookingHotelAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementAndBookingHotelAppApplication.class, args);
+	}
+
+}
